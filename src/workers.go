cd4ac3a7f60fbff32c269a54cbe03207ccc4a9ea
Adrián López <adrian@undefinedlabs.com>
2024-06-10 17:06:04 +0200

    Add stats

--- src/workers.go
+++ src/workers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log/slog"
 	"sync"
+	"time"
 
 	"github.com/cespare/xxhash"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -15,8 +16,12 @@ type WorkFunc func(int, QueryParams) // so we can swap it out from tests
 type workerPool struct {
 	dbpool     *pgxpool.Pool
 	wg         sync.WaitGroup
-	inputChans []chan QueryParams
+	inputChans []chan QueryParams // one per worker
 	workFunc   WorkFunc
+	// for stats
+	workTimes [][]time.Duration // one slice per worker to be appended after finish
+	startTime time.Time
+	totalTime time.Duration
 }
 
 // NewWorkerPool creates a new worker pool
@@ -36,10 +41,12 @@ func defaulWorkFunc(dbpool *pgxpool.Pool) WorkFunc {
 func (w *workerPool) Start(numWorkers int) {
 	w.wg.Add(numWorkers)
 	w.inputChans = make([]chan QueryParams, numWorkers)
+	w.workTimes = make([][]time.Duration, numWorkers)
 	for i := 0; i < numWorkers; i++ {
 		w.inputChans[i] = make(chan QueryParams, workerChannelSize)
 		go w.runWorker(i)
 	}
+	w.startTime = time.Now()
 }
 
 func (w *workerPool) Stop() {
@@ -47,13 +54,18 @@ func (w *workerPool) Stop() {
 		close(ch)
 	}
 	w.wg.Wait()
+	w.totalTime = time.Since(w.startTime)
 }
 
 func (w *workerPool) runWorker(workerIndex int) {
 	defer w.wg.Done()
+	workTimes := []time.Duration{}
 	for params := range w.inputChans[workerIndex] {
+		start := time.Now()
 		w.workFunc(workerIndex, params)
+		workTimes = append(workTimes, time.Since(start))
 	}
+	w.workTimes[workerIndex] = workTimes
 }
 
 // QueryCallback queues the params to run asynchronously in the worker pool
@@ -69,4 +81,12 @@ func (w *workerPool) routeQuery(params QueryParams) {
 	workerNum := hash % uint64(len(w.inputChans))
 	slog.With("route_key", params.RouteKey(), "worker_num", workerNum).Debug("forwarding params to worker")
 	w.inputChans[workerNum] <- params
+}
+
+func (w *workerPool) GetStats() *Stats {
+	st := &Stats{TotalTime: w.totalTime}
+	for _, workTimes := range w.workTimes {
+		st.Durations = append(st.Durations, workTimes...)
+	}
+	return st
 }
--- src/workers_test.go
+++ src/workers_test.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"sync"
+	"sync/atomic"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestWorkerPool(t *testing.T) {
-	require := require.New(t)
-
-	// prepare an callback that keeps track of which workers receive the route keys
+func TestWorkerPoolRouting(t *testing.T) {
+	// prepare a callback that keeps track of which workers receive the route keys
 	workerSeenForEachRouteKey := map[string]map[int]struct{}{}
 	var lock sync.RWMutex
 	fn := func(workerIndex int, params QueryParams) {
@@ -28,10 +27,27 @@ func TestWorkerPool(t *testing.T) {
 	worker.Start(10)
 
 	err := StreamParamsFilePath(csvFilePath, worker.QueryCallback())
-	require.NoError(err)
+	require.NoError(t, err)
 
 	worker.Stop()
 	for routeKey, seenWorkers := range workerSeenForEachRouteKey {
-		require.Len(seenWorkers, 1, "routekey %s received by more than one worker", routeKey)
+		require.Len(t, seenWorkers, 1, "routekey %s received by more than one worker", routeKey)
 	}
+}
+
+func TestWorkerPoolStats(t *testing.T) {
+	var calls atomic.Int32
+	fn := func(int, QueryParams) {
+		calls.Add(1)
+	}
+	worker := &workerPool{workFunc: fn}
+	worker.Start(10)
+
+	err := StreamParamsFilePath(csvFilePath, worker.QueryCallback())
+	require.NoError(t, err)
+
+	worker.Stop()
+	stats := worker.GetStats()
+	require.NotZero(t, stats.TotalTime)
+	require.Len(t, stats.Durations, int(calls.Load()))
 }
